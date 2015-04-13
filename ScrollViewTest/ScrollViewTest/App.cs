c6ca1018081a273af8c74a4b93aabcc6d488ec05
Abhiras <abhirasmanu@gmail.com>
2015-04-13 16:08:25 +0200

    ScrollViewTest

---
+++ ScrollViewTest/ScrollViewTest/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ScrollViewTest
+{
+	public class App
+	{
+	   
+		public static Page GetMainPage ()
+		{
+
+            return new NavigationPage(new SampleScrollView());
+		}
+	}
+}
+
---
+++ ScrollViewTest/ScrollViewTest/Views/SampleScrollView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ScrollViewTest
+{	
+	public partial class SampleScrollView : ContentPage
+	{
+        public SampleScrollView()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
